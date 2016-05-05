434b99c19710b909baac2d444842c2e0e9c24644
sepehrvakili <sdvakili@gmail.com>
2016-05-05 16:08:13 -0700

    Fix click to remove from song queue

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,27 +1,14 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
-
-  // tagName: 'tr',
-
-  // template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
-
-  // events: {
-  //   'click': function() {
-  //     this.model.play();
-  //     this.model.enqueue();
-  //   }
-  // },
-
-  // render: function() {
-  //   return this.$el.html(this.template(this.model.attributes));
-  // }
+  events: {
+    'click': function() {
+      this.model.dequeue();
+    }
+  },
 
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this.$el;
   }
-
-
 });
